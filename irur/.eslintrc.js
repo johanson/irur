2675bfddda140962e3c53d2eeca529cc9afb9b58
Rasmus Johanson <rasmusjohanson@gmail.com>
2020-08-18 01:28:07 +0300

    Update eslint rules

--- irur/.eslintrc.js
+++ irur/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-restricted-syntax': 'off',
-    'guard-for-in': 'off'
+    'guard-for-in': 'off',
+    'default-case': 'off',
   },
 };
