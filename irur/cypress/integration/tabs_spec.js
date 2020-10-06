64f7646577289d5d472c2e34d9f5de5358a62193
Rasmus Johanson <rasmusjohanson@gmail.com>
2020-10-06 20:53:49 +0300

    Add tabs tests

---
+++ irur/cypress/integration/tabs_spec.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+describe('Tabs component', () => {
+  it('Create a new tab', () => {
+    cy.visit('/');
+    cy.get('#loading').should('not.be.visible');
+    cy.wait(500);
+    cy.window().then(win => {
+      win.app.__vue__.resetDB();
+    });
+    cy.get('.tab').rightclick();
+    cy.get('.v-context a[data-name=add]').click();
+    cy.get('.tab.active input')
+      .clear()
+      .type('FooBar{enter}');
+    cy.get('.tab.active span').contains('FooBar');
+  });
+
+  it('Rename a tab', () => {
+    cy.get('.tab span')
+      .contains('FooBar')
+      .rightclick();
+    cy.get('.v-context a[data-name=rename]').click();
+    cy.get('.tab.active input')
+      .clear()
+      .type('BarFoo{enter}');
+    cy.get('.tab.active span').contains('BarFoo');
+  });
+
+  it('Delete a tab', () => {
+    cy.get('.tab span')
+      .contains('BarFoo')
+      .rightclick();
+    cy.get('.v-context a[data-name=remove]').click();
+    cy.get('#prompt .confirm').click();
+    cy.get('a#undo').should('be.visible');
+    cy.get('#tabs')
+      .find('.tab')
+      .should('have.length', 1);
+  });
+});
