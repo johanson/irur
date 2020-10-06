a0f063fbf9a74dd7de55257a10beb9f2f7f308c9
Rasmus Johanson <rasmusjohanson@gmail.com>
2020-10-06 21:04:46 +0300

    Format settings tests

--- irur/cypress/integration/settings_spec.js
+++ irur/cypress/integration/settings_spec.js
@@ -1,30 +1,19 @@
 /* eslint-disable no-undef */
-const loader = '#loading';
-const settings = '#settings';
-const settingsBtn = `${settings} .btn`;
-const settingsWindow = `${settings} .window`;
-const settingsWindowCloseBtn = `${settings} .close`;
-const saveBtn = `${settings} .save`;
-const textarea = `${settingsWindow} textarea`;
-const prompt = '#prompt';
-const promptConfirmButton = `${prompt} .confirm`;
-const remote = '#remote';
-
 describe('Settings component', () => {
   it('Makes settings window visible when the settings button is clicked', () => {
     cy.visit('/');
-    cy.get(loader).should('not.be.visible');
+    cy.get('#loading').should('not.be.visible');
     cy.wait(500);
     cy.window().then(win => {
       win.app.__vue__.resetDB();
     });
-    cy.get(settingsBtn).click();
-    cy.get(settingsWindow);
+    cy.get('#settings .btn').click();
+    cy.get('#settings .window');
   });
 
   it('Makes manual changes to json', () => {
-    cy.get(textarea).clear();
-    cy.get(textarea).type(
+    cy.get('#settings .window textarea').clear();
+    cy.get('#settings .window textarea').type(
       JSON.stringify({
         default: {
           name: 'Default',
@@ -47,21 +36,21 @@ describe('Settings component', () => {
   });
 
   it('Saves changes and makes the prompt window visible', () => {
-    cy.get(saveBtn).click();
-    cy.get(prompt).should('be.visible');
+    cy.get('#settings .save').click();
+    cy.get('#prompt').should('be.visible');
   });
 
   it('Confirms changes are visible, corresponding to DOM/db', () => {
-    cy.get(promptConfirmButton).click();
-    cy.get(remote)
+    cy.get('#prompt .confirm').click();
+    cy.get('#remote')
       .find('.knob')
       .should('have.length', 2); // 1  + add new knob
   });
 
   it('Opens and closes settings window', () => {
-    cy.get(settingsBtn).click();
-    cy.get(settingsWindow);
-    cy.get(settingsWindowCloseBtn).click();
-    cy.get(settingsWindow).should('not.be.visible');
+    cy.get('#settings .btn').click({ force: true });
+    cy.get('#settings .window');
+    cy.get('#settings .close').click();
+    cy.get('#settings .window').should('not.be.visible');
   });
 });
